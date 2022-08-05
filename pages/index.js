ff02bb1d33705007a67cc0432913d3316fb8a1ed
khw970421 <khw970421@kakao.com>
2022-08-05 20:07:06 +0900

    ✨ feat : Header 목업 구현

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,55 @@
 import styled from "styled-components";
 import Camp from "../component/Camp";
 import CampContainer from "../component/CampContainer";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Home() {
   return (
-    <Body>
-      <CampContainer />
-    </Body>
+    <div>
+      <Header>
+        <ImgContainer>
+          <Img src="mainlogo.png"></Img>
+        </ImgContainer>
+        <Input placeholder="어디로 갈까?"></Input>
+        <HamburgerContainer>
+          <GiHamburgerMenu size="50" />
+        </HamburgerContainer>
+      </Header>
+      <Body>{/* <CampContainer /> */}</Body>
+    </div>
   );
 }
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ImgContainer = styled.div`
+  width: 10vw;
+  margin: 20px;
+`;
+
+const Img = styled.img`
+  width: 100%;
+`;
+
+const HamburgerContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 10vw;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 0.3px solid;
+  border-radius: 24px;
+  width: 30vw;
+  height: 50px;
+  margin: 20px;
+  padding: 10px;
+`;
 
 const Body = styled.div`
   display: flex;
