c702b34836e53f2d95750fee23b1ed7b890adf59
khw970421 <khw970421@kakao.com>
2022-08-10 21:20:42 +0900

    💄 styles : Input 검색결과 스타일 수정

--- component/Input.jsx
+++ component/Input.jsx
@@ -23,58 +23,100 @@ const Input = ({
   };
 
   return (
-    <InputContainer>
+    <InputContainer
+      width={width}
+      height={height}
+      borderRadius={borderRadius}
+      searchArr={searchArr}
+    >
       <InputTag
         onChange={debounce}
         width={width}
         height={height}
         borderRadius={borderRadius}
+        searchArr={searchArr}
       ></InputTag>
       {searchArr.length !== 0 && (
-        <Ul height={height} borderRadius={borderRadius}>
-          {searchArr.map((search) => {
-            return (
-              <Li
-                key={search}
-                id="backgroundWhite"
-                onClick={clickSearch}
-                width={width}
-                height={height}
-              >
-                {search}
-              </Li>
-            );
-          })}
-        </Ul>
+        <>
+          <Ul height={height} borderRadius={borderRadius}>
+            {searchArr.map((search) => {
+              return (
+                <Li
+                  key={search}
+                  id="backgroundWhite"
+                  onClick={clickSearch}
+                  width={width}
+                  height={height}
+                >
+                  {search}
+                </Li>
+              );
+            })}
+            <Li
+              id="backgroundWhite"
+              width={width}
+              height={5}
+              borderRadius={borderRadius}
+            ></Li>
+          </Ul>
+        </>
       )}
     </InputContainer>
   );
 };
 
 const InputContainer = styled.div`
+  box-sizing: content-box;
   display: flex;
   flex-direction: column;
   position: relative;
+  justify-content: center;
+  align-items: center;
+  border-radius: ${({ borderRadius, searchArr }) =>
+    searchArr.length === 0
+      ? `${borderRadius}px`
+      : ` ${borderRadius}px ${borderRadius}px 0px 0px`};
+  border: 1px solid;
+  width: ${({ width }) => `${width + 5}vw`};
+  height: auto;
 `;
 
 const InputTag = styled.input`
   width: ${({ width }) => `${width}vw`};
   height: ${({ height }) => `${height}px`};
-  border-radius: ${({ borderRadius }) => `${borderRadius}px`};
-  border: 0.3px solid;
-  padding: 10px;
+  border: 0px;
+  :focus {
+    outline: none;
+  }
 `;
 const Ul = styled.ul`
   position: absolute;
-  margin-top: ${({ height }) => `${height}px`};
+  top: ${({ height }) => `${height}px`};
   padding: 0px;
-  border: 1px solid;
-  border-radius: ${({ borderRadius }) => `${borderRadius}px`};
+  margin: 0px;
+  border-radius: ${({ borderRadius }) =>
+    `0px 0px ${borderRadius}px ${borderRadius}px`};
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-left-width: 1px;
+  border-left-style: solid;
+  border-right-width: 1px;
+  border-right-style: solid;
 `;
 const Li = styled.li`
-  width: ${({ width }) => `${width}vw`};
+  width: ${({ width }) => `${width + 5}vw`};
   height: ${({ height }) => `${height}px`};
   list-style: none;
   padding: 10px;
+  border-radius: ${({ borderRadius = 0 }) =>
+    `0px 0px ${borderRadius}px ${borderRadius}px`};
 `;
+
+// const Side = styled.div`
+//   border-radius: ${({ borderRadius }) =>
+//     `0px 0px ${borderRadius}px ${borderRadius}px`};
+//   border: 1px solid;
+//   height: 10px;
+//   width: ${({ width }) => `${width + 5}vw`};
+// `;
 export default Input;
