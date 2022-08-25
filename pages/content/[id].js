9ca3a79b797b5c185ba5ba6478ee0f38bd8dac34
khw970421 <khw970421@kakao.com>
2022-08-25 11:50:36 +0900

    ✨ feat : Slider 적용

--- pages/content/[id].js
+++ pages/content/[id].js
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/dist/client/router";
 import { getLocationBasedList, getImageList } from "../../core/api/axios";
+import styled from "styled-components";
+import Slider from "../../component/Slider";
 
 const content = () => {
   const router = useRouter();
   const [content, setContent] = useState([]);
+  const [imageLists, setImageLists] = useState([]);
 
   useEffect(() => {
     if (!router.isReady) return;
     locationBasedList(1, router.query.mapX, router.query.mapY);
+    imageList(1, router.query.id);
   }, [router.isReady]);
 
   async function locationBasedList(pageNo = 1, mapX, mapY) {
@@ -16,12 +20,36 @@ const content = () => {
     setContent(data[0]);
   }
 
+  async function imageList(pageNo = 1, contentId) {
+    const data = await getImageList(pageNo, contentId);
+    setImageLists(data);
+  }
+
   return (
     <>
-      <div>About {router.query.id}</div>
-      <div>{content.facltNm}</div>
+      <Body>
+        <Main>
+          <Title>🏕️ {content.facltNm}</Title>
+          <Slider imgs={imageLists} />
+        </Main>
+      </Body>
     </>
   );
 };
+
+const Body = styled.div`
+  width: 100%;
+  min-height: 100vh;
+`;
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: calc(100vw - 22vw * 2);
+  height: auto;
+  margin: 0vw 22vw;
+`;
+
+const Title = styled.div``;
 
 export default content;
