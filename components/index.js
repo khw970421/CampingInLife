515ee235dd89a5490299b9e4b68f04ee53bb35b0
khw970421 <khw970421@kakao.com>
2022-10-30 13:09:25 +0900

    ♻️ refactor : 컴포넌트 폴더구조 변경

---
+++ components/index.js
@@ -0,0 +1,9 @@
+export { default as Header } from "@/components/Semantic/Header/Header";
+export { default as Footer } from "@/components/Semantic/Footer/Footer";
+
+export { default as Button } from "@/components/Button/Button";
+export { default as CampTile } from "@/components/CampTile/CampContainer";
+export { default as Intro } from "@/components/Intro/Intro";
+export { default as KakaoAPI } from "@/components/KakaoAPI/KakaoAPI";
+export { default as SelectBox } from "@/components/SelectBox/SelectBox";
+export { default as Slider } from "@/components/Slider/Slider";
