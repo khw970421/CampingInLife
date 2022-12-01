cbb38ba871a4642a4f0054c0626aa2d188c4319e
JunilHwang <junil.h@kakao.com>
2022-12-01 16:30:19 +0900

    refactor: searchList api 개선

--- pages/api/searchList.js
+++ pages/api/searchList.js
@@ -1,15 +1,12 @@
-import axios from "axios";
-import getURL from "./url.js";
+import {getServerSideSearchList} from "@/core/api/axios";
+
 let result;
 export default async function handler(req, res) {
-  const { query } = req;
-  const queryStr = Object.entries(query)
-    .map(([key, value]) => `&${key}=${value}`)
-    .join("");
+  const {pageNo, keyword} = req.query;
   try {
-    result = await axios.get(`${getURL("searchList")}${queryStr}`);
+    result = await getServerSideSearchList(pageNo, keyword);
     res.status(200).json(result.data);
   } catch (err) {
-    res.status(500).json({ error: "failed to load data" });
+    res.status(500).json({error: "failed to load data"});
   }
 }
