0e69a9a888c0d138b276fe80088883044865340f
khw970421 <khw970421@kakao.com>
2023-09-12 19:08:02 +0900

    footer_data 분리 및 불필요 div 수정

--- components/Semantic/Footer/Footer.tsx
+++ components/Semantic/Footer/Footer.tsx
@@ -1,37 +1,39 @@
 import React from "react";
 import styled from "styled-components";
 
+const footer_data = {
+  'copyright': 'COPYRIGHT 2022 BY CampInLife ALL RIGHT RESERVED',
+  'information': {
+    'phone': '전화 : 010-3266-1140',
+    'email': '이메일 : khw970421@kakao.com',
+    'github': 'GitHub : khw970421',
+  }
+}
+
 const Footer = () => {
+  const { copyright, information } = footer_data
   return (
     <FooterContainer>
-      <div id="displayMax700">
-        COPYRIGHT 2022 BY CampInLife ALL RIGHT RESERVED
-      </div>
-      <div>
-        <Information>💻 개발자 정보</Information>
-        <Phone>📱 &nbsp;&nbsp;전화 : 010-3266-1140 </Phone>
-        <div>✉️ 이메일 : khw970421@kakao.com</div>
-        <div>📜 GitHub : khw970421</div>
-      </div>
+      <CopyRight>{copyright}</CopyRight>
+      <Information>
+        <h3>💻 개발자 정보</h3>
+        {Object.values(information).map(data => <span key={data}>{data}</span>)}
+      </Information>
     </FooterContainer>
   );
 };
 
-const FooterContainer = styled.div`
+const FooterContainer = styled.footer`
   display: flex;
   justify-content: space-around;
   align-items: center;
   padding: 5px 0px;
 `;
 
-const Information = styled.div`
-  font-weight: bold;
-  font-size: 1em;
-  padding: 10px 0px;
-`;
-
-const Phone = styled.div`
-  white-space: pre;
-`;
+const CopyRight = styled.p``
+const Information = styled.p`
+  display:flex;
+  flex-direction:column;
+`
 
 export default Footer;
