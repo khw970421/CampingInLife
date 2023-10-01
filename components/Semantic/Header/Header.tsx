fdc50082a91d18fd3d1220747d24f24fe799b69f
khw970421 <khw970421@kakao.com>
2023-10-01 13:57:12 +0900

    Header 검색부분 클릭시 전체화면으로 분리

--- components/Semantic/Header/Header.tsx
+++ components/Semantic/Header/Header.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { GiHamburgerMenu } from "react-icons/gi";
 import Input from "@/components/Semantic/Header/Input";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import { SearchCamping } from "@/core/utils/types.d";
-
+import { ImSearch } from "react-icons/im";
 
 interface HeaderProps {
-  isInputExist: boolean;
+  isPossibleSearch?: boolean;
   searchCamping: SearchCamping[];
   isSearching: boolean;
   changeInputValue: (event: React.ChangeEvent) => void;
@@ -22,7 +22,7 @@ interface HeaderProps {
 }
 
 export default function Header({
-  isInputExist = false,
+  isPossibleSearch = true,
   searchCamping,
   changeInputValue,
   checkSearchPressEnter,
@@ -30,14 +30,29 @@ export default function Header({
   isSearching,
 }: HeaderProps) {
   const router = useRouter();
+  const [isOpenSearchBar, setIsOpenSearchBar] = useState(false)
+  const handleOpenMenu = () => {
+    //TODO: 메뉴 
+    alert('아직 존재하지 않는 기능입니다.')
+  }
+
+  const handleOpenSearchBar = () => {
+    setIsOpenSearchBar(true)
+    document.body.style.overflow = 'hidden'
+  }
+
+  const handleCloseSearchBar = () => {
+    setIsOpenSearchBar(false)
+    document.body.style.overflow = 'auto'
+  }
+
   return (
     <HeaderContainer>
       <ImgContainer onClick={() => router.push("/")}>
-        <label htmlFor="search">
-          <Image src="/mainlogo.png" alt="main image" width={200} height={100} objectFit="contain"></Image>
-        </label>
+        <Image src="/mainlogo.png" alt="main image" width={200} height={100} ></Image>
       </ImgContainer>
-      {isInputExist && (
+
+      {isOpenSearchBar && (
         <Input
           id="search"
           searchCamping={searchCamping}
@@ -45,11 +60,17 @@ export default function Header({
           changeInputValue={changeInputValue}
           checkSearchPressEnter={checkSearchPressEnter}
           handleClearSearchData={handleClearSearchData}
+          closeSearchBar={handleCloseSearchBar}
         ></Input>
       )}
-      <HamburgerContainer>
-        <GiHamburgerMenu size="50" />
-      </HamburgerContainer>
+      <Menu>
+        {isPossibleSearch && <ImSearchContainer onClick={handleOpenSearchBar}>
+          <ImSearch size="40" />
+        </ImSearchContainer>}
+        <HamburgerContainer onClick={handleOpenMenu}>
+          <GiHamburgerMenu size="50" />
+        </HamburgerContainer>
+      </Menu>
     </HeaderContainer>
   );
 }
@@ -58,24 +79,26 @@ const HeaderContainer = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  position:relative;
 `;
 
 const ImgContainer = styled.div`
   width: 10vw;
-  min-width: 50px;
+  min-width: 200px;
   margin: 20px;
   cursor: pointer;
   height:100px;
   overflow: hidden;
 `;
 
-const Img = styled.img`
-  width: 100%;
+const Menu = styled.div`
+  display:flex;
+  align-items:center;
+  gap:1rem;
+`
+
+const ImSearchContainer = styled.div`
 `;
 
 const HamburgerContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  width: 10vw;
-  margin: 20px;
 `;
